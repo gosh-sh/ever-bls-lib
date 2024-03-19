5ffe481ede4ef228344973f6dcc334709d55517a
Alexander <alexander.s@gosh.sh>
2024-03-19 13:49:50 +0300

    Updated bin to work without args and parse arguments with clap

--- src/bin/bls_keypair_gen.rs
+++ src/bin/bls_keypair_gen.rs
@@ -1,18 +1,33 @@
 // 2022-2024 (c) Copyright Contributors to the GOSH DAO. All rights reserved.
 //
+use gosh_bls_lib::bls::gen_bls_key_pair;
 use gosh_bls_lib::bls::gen_bls_key_pair_based_on_key_material;
 use gosh_bls_lib::bls::BLS_SECRET_KEY_LEN;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Hex string with 32 bytes of key material for bls
+    #[arg(short, long)]
+    key_material: Option<String>
+}
+
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    assert!(args.len() > 1, "Secret key should be passed as an argument");
+    let args: Args = Args::parse();
 
-    let mut secret_key = [0_u8; BLS_SECRET_KEY_LEN];
-    hex::decode_to_slice(args[1].trim_start_matches("0x"), &mut secret_key)
-        .expect("Failed to decode secret key from hex");
+    let (public, secret) = if let Some(key_material) = args.key_material {
+        let mut secret_key = [0_u8; BLS_SECRET_KEY_LEN];
+        hex::decode_to_slice(key_material.trim_start_matches("0x"), &mut secret_key)
+            .expect("Failed to decode secret key from hex");
 
-    let (public, secret) = gen_bls_key_pair_based_on_key_material(&secret_key)
-        .expect("Failed to generate BLS key pair");
+        gen_bls_key_pair_based_on_key_material(&secret_key)
+            .expect("Failed to generate BLS key pair")
+    } else {
+        gen_bls_key_pair().expect("Failed to generate BLS key pair")
+    };
+
     println!(
         r#"{{
   "public": "{}",
